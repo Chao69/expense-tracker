e3cae8bf177bb7d5cfac5f6076b040cc3229a6ce
chao69 <star010631@gmail.com>
2021-07-21 23:17:13 +0800

    feat: add main node.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+app.get('/', (req, res) => {
+  res.send('This is an expense-tracker project on alpha camp!!!')
+})
+
+app.listen(3000, () => {
+  console.log('Express is running on http://localhost:3000')
+})
