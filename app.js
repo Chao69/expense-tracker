ad02d17294940898b28c14d1065721844dd34015
chao69 <star010631@gmail.com>
2021-07-24 20:04:34 +0800

    feat: add sort by date && a[[.post('new')

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Record = require('./models/record')
 const Category = require('./models/category')
-
+const bodyParser = require('body-parser')
 const app = express()
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,10 +21,13 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // use data seed to render home page
 app.get('/', (req, res) => {
   Record.find()
     .lean()
+    .sort({ date: 'asc' })
     .then((records) => {
       let totalAmount = 0
       records.forEach(record => {
@@ -35,8 +38,22 @@ app.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.get('/expense/new', (req, res) => {
-  res.render('new')
+app.get('/new', (req, res) => {
+  Category.find()
+    .lean()
+    .then(categorys => {
+      const categoryList = []
+      categoryList.push(...categorys)
+      res.render('new', { categoryList })
+    })
+})
+
+app.post('/new', (req, res) => {
+  const { name, date, category, amount } = req.body
+  console.log(res.body)
+  Record.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
 })
 
 app.listen(3000, () => {
