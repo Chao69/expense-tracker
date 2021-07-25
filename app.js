5f96a3dd28f0a01cf415fda298da896636a4dc10
chao69 <star010631@gmail.com>
2021-07-25 19:20:40 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -93,6 +93,15 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.error(error))
 })
 
+// define delete function
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
 app.listen(3000, () => {
   console.log('Express is running on http://localhost:3000')
 })
